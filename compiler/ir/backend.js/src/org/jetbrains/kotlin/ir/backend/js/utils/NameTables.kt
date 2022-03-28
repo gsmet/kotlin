9259a83ef295072d3336359979db94b4fc179f6a
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-03-28 12:17:14 +0300

    [JS IR] Change prefix for type arguments

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/NameTables.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/NameTables.kt
@@ -129,7 +129,7 @@ fun jsFunctionSignature(declaration: IrFunction, context: JsIrBackendContext): S
 
     declaration.extensionReceiverParameter?.let {
         nameBuilder.append("_r$${it.type.eraseGenerics(context.irBuiltIns).asString()}")
-        nameBuilder.append("_r$${it.type.typeArgumentsToString(context)}")
+        nameBuilder.append("_rgen$${it.type.typeArgumentsToString(context)}")
     }
     declaration.valueParameters.ifNotEmpty {
         joinTo(nameBuilder, "") { "_${it.type.eraseGenerics(context.irBuiltIns).asString()}" }
@@ -142,7 +142,7 @@ fun jsFunctionSignature(declaration: IrFunction, context: JsIrBackendContext): S
         // they are binary incompatible with supertypes.
         if (context.inlineClassesUtils.isTypeInlined(it) || it.isUnit()) {
             nameBuilder.append("_ret$${it.eraseGenerics(context.irBuiltIns).asString()}")
-            nameBuilder.append("_ret$${it.typeArgumentsToString(context)}")
+            nameBuilder.append("_retgen$${it.typeArgumentsToString(context)}")
         }
     }
 
@@ -162,6 +162,7 @@ fun IrSimpleType.collectTypeArguments(irBuiltIns: IrBuiltIns): List<IrType> {
     val classifier = classifierOrNull
     if (classifier !is IrClassSymbol) return emptyList()
     val result = mutableListOf<IrType>()
+    result.add(this.eraseGenerics(irBuiltIns))
     arguments.forEach {
         if (it is IrSimpleType) {
             it.collectTypeArguments(irBuiltIns).forEach {
@@ -170,7 +171,6 @@ fun IrSimpleType.collectTypeArguments(irBuiltIns: IrBuiltIns): List<IrType> {
         }
     }
 
-    result.add(0, this.eraseGenerics(irBuiltIns))
     return result
 }
 
